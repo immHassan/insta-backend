adf26b08da78ab38bc8b330afc0959371ff6379e
Muhammad Hassan <30412697+immHassan@users.noreply.github.com>
2024-02-08 02:00:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,83 @@ const axios = require("axios");
 
 const app = express();
 
-// Define the Instagram API endpoint URL and headers
-const apiUrl = "https://i.instagram.com/api/v1/users/52864197/info/";
-const headers = {
-  cookie:
-    'ig_did=144CB627-09B1-405F-9F0F-6D1ACFAA4428; ig_nrcb=1; mid=ZcJgqwALAAF7JhFWmyOTCiBbGBlq; datr=0WfCZefcFVhZ9nynnxPret6H; ps_l=0; ps_n=0; ds_user_id=59541738154; csrftoken=3zTizQvuO7tb5T17ht4j4NfLksGNBsqI; sessionid=59541738154%3A2jzoZYIywD0Vu9%3A17%3AAYf4QSv7gG9Sp1YjevLXjtMZnOa1FMRJYovgX9UXCA; rur="LDC\05459541738154\0541738833932:01f7491e829e3d566437f4d08c6ece540c7e4befadebb707e923578c41c7886ee2285927"',
-  "user-agent":
-    "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
-};
-
 // Define a route to fetch Instagram user info
-app.get("/instagram/user/info", (req, res) => {
-  // Make the GET request to the Instagram API
+
+// To get Data from instagram user Id
+app.get("/api/instagram/user-pictures/:userId", (req, res) => {
+  const { userId } = req.params; // Get the userId from the request parameters
   axios
-    .get(apiUrl, { headers })
+    .get(`https://i.instagram.com/api/v1/users/${userId}/info/`, {
+      headers: {
+        authority: "i.instagram.com",
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        cookie:
+          'mid=ZcPjRgALAAHLVmnHQIxVqe1zxKDX; ig_did=2BDBD27A-E676-4176-AF10-5022D896AD4F; ig_nrcb=1; ps_l=0; ps_n=0; datr=TOPDZYjoIan2799ltR9LkV_I; csrftoken=K8zIkvsTmvIe3agqQtHnbEEfCaPQ3kC8; ds_user_id=59541738154; sessionid=59541738154%3AXqcnOafdo7126y%3A6%3AAYfvUE3Cjaf_E1u2TQZbpF-Zomq5Ac4gIDUwAqKRLw; rur="LDC\05459541738154\0541738872561:01f719dab35a860f64b1dee065b2fdf0e02a8fb84453eaf7d7fca33d37f3f2aabe0c106f"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
+      },
+    })
     .then((response) => {
-      // Send the response data back to the client
-      res.json(response.data);
+      let data = [];
+
+      data = response.data.user.hd_profile_pic_versions;
+      data = [...data, response.data.user.hd_profile_pic_url_info];
+
+      res.json({
+        profile_pictures: data,
+        message: "Data fetched successfully",
+        success: true,
+      });
     })
     .catch((error) => {
-      // Handle errors
-      console.error("Error fetching data:", error);
-      res.status(500).json({
-        error: "An error occurred while fetching data from Instagram API",
+      console.error(error);
+      res.json({ data: [], message: "something went wrong", success: false });
+    });
+});
+
+// To get User Id from Username
+app.get("/api/instagram/user-id/:userName", (req, res) => {
+  const { userName } = req.params; // Get the userId from the request parameters
+  axios
+    .get(
+      `https://i.instagram.com/api/v1/users/web_profile_info/?username=${userName}`,
+      {
+        headers: {
+          authority: "i.instagram.com",
+          accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "accept-language": "en-US,en;q=0.9",
+          "cache-control": "max-age=0",
+          cookie:
+            'mid=ZcPjRgALAAHLVmnHQIxVqe1zxKDX; ig_did=2BDBD27A-E676-4176-AF10-5022D896AD4F; ig_nrcb=1; ps_l=0; ps_n=0; datr=TOPDZYjoIan2799ltR9LkV_I; csrftoken=K8zIkvsTmvIe3agqQtHnbEEfCaPQ3kC8; ds_user_id=59541738154; sessionid=59541738154%3AXqcnOafdo7126y%3A6%3AAYfvUE3Cjaf_E1u2TQZbpF-Zomq5Ac4gIDUwAqKRLw; rur="LDC\05459541738154\0541738872561:01f719dab35a860f64b1dee065b2fdf0e02a8fb84453eaf7d7fca33d37f3f2aabe0c106f"',
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1",
+          "user-agent":
+            "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
+        },
+      }
+    )
+    .then((response) => {
+      res.json({
+        userId: response.data.data.user.id,
+        message: "Data fetched successfully",
+        success: true,
       });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.json({ data: [], message: "something went wrong", success: false });
     });
 });
 
